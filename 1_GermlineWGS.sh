8a55aaa8686229bfc10a6307013d4a5bbfc176b0
mcgml <mcgml@cardiff.ac.uk>
2017-05-16 16:55:25 +0100

    Updated mapping

--- 1_GermlineWGS.sh
+++ 1_GermlineWGS.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#PBS -l walltime=40:00:00
+#PBS -l walltime=999:00:00
 #PBS -l ncpus=12
 set -euo pipefail
 PBS_O_WORKDIR=(`echo $PBS_O_WORKDIR | sed "s/^\/state\/partition1//" `)
 cd $PBS_O_WORKDIR
 
 #Description: Germline Enrichment Pipeline (Illumina paired-end). Not for use with other library preps/ experimental conditions.
-#Author: Matt Lyon, All Wales Medical Genetics Lab
+#Author: Matt Lyon, Harriet Jackson All Wales Medical Genetics Lab
 #Mode: BY_SAMPLE
-version="1.8.7"
+version="1.0.0"
 
 # Script 1 runs in sample folder, requires fastq files split by lane
 
 #load sample & pipeline variables
 . *.variables
-. /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel".variables
 
 countQCFlagFails() {
     #count how many core FASTQC tests failed
@@ -31,17 +30,17 @@ countQCFlagFails() {
 rawSequenceQuality=PASS
 
 #convert FASTQ to uBAM & add RGIDs
-for fastqPair in $(ls "$sampleId"_S*.fastq.gz | cut -d_ -f1-3 | sort | uniq); do
+for fastqPair in $(ls *.fastq.gz | cut -d_ -f1-2 | sort | uniq); do
 
     #parse fastq filenames
-    laneId=$(echo "$fastqPair" | cut -d_ -f3)
-    read1Fastq=$(ls "$fastqPair"_R1_*fastq.gz)
-    read2Fastq=$(ls "$fastqPair"_R2_*fastq.gz)
+    laneId=$(echo "$fastqPair" | cut -d_ -f2)
+    read1Fastq=$(ls "$fastqPair"_1_.fastq.gz)
+    read2Fastq=$(ls "$fastqPair"_2_.fastq.gz)
 
     #trim adapters
     /share/apps/cutadapt-distros/cutadapt-1.9.1/bin/cutadapt \
-    -a "$read1Adapter" \
-    -A "$read2Adapter" \
+    -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC \
+    -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA \
     -m 35 \
     -o "$seqId"_"$sampleId"_"$laneId"_R1.fastq \
     -p "$seqId"_"$sampleId"_"$laneId"_R2.fastq \
@@ -60,14 +59,12 @@ for fastqPair in $(ls "$sampleId"_S*.fastq.gz | cut -d_ -f1-3 | sort | uniq); do
     F1="$seqId"_"$sampleId"_"$laneId"_R1.fastq \
     F2="$seqId"_"$sampleId"_"$laneId"_R2.fastq \
     O="$seqId"_"$sampleId"_"$laneId"_unaligned.bam \
-    QUALITY_FORMAT=Standard \
     READ_GROUP_NAME="$seqId"_"$laneId"_"$sampleId" \
     SAMPLE_NAME="$sampleId" \
     LIBRARY_NAME="$worklistId"_"$sampleId"_"$panel" \
     PLATFORM_UNIT="$seqId"_"$laneId" \
     PLATFORM="ILLUMINA" \
-    SEQUENCING_CENTER="IMG" \
-    PREDICTED_INSERT_SIZE="$expectedInsertSize" \
+    SEQUENCING_CENTER="WTCHG" \
     SORT_ORDER=queryname \
     MAX_RECORDS_IN_RAM=2000000 \
     TMP_DIR=/state/partition1/tmpdir
@@ -85,7 +82,7 @@ for fastqPair in $(ls "$sampleId"_S*.fastq.gz | cut -d_ -f1-3 | sort | uniq); do
     rm "$seqId"_"$sampleId"_"$laneId"_R1.fastq "$seqId"_"$sampleId"_"$laneId"_R2.fastq *_fastqc.zip *_fastqc.html
 
 done
-
+exit
 #merge lane bams
 /share/apps/jre-distros/jre1.8.0_101/bin/java -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx8g -jar /share/apps/picard-tools-distros/picard-tools-2.8.3/picard.jar MergeSamFiles \
 $(ls "$seqId"_"$sampleId"_*_unaligned.bam | sed 's/^/I=/' | tr '\n' ' ') \
