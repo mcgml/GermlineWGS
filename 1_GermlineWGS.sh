ec5345c2df168985443875e8fd7f75fd2cee8557
mcgml <mcgml@cardiff.ac.uk>
2017-06-14 16:07:12 +0100

    updated HC pcr model

--- 1_GermlineWGS.sh
+++ 1_GermlineWGS.sh
@@ -227,7 +227,7 @@ TMP_DIR=/state/partition1/tmpdir
 --emitRefConfidence GVCF \
 -nct 12 \
 -L 1 -L 2 -L 3 -L 4 -L 5 -L 6 -L 7 -L 8 -L 9 -L 10 -L 11 -L 12 -L 13 -L 14 -L 15 -L 16 -L 17 -L 18 -L 19 -L 20 -L 21 -L 22 -L X -L Y -L MT \
-$(awk -vpcr="$pcr" 'BEGIN {if (pcr) print "-pcr_indel_model NONE"; else print "-pcr_indel_model CONSERVATIVE"}') \
+$(awk -vpcr="$pcr" 'BEGIN {if (pcr) print "--pcr_indel_model CONSERVATIVE"; else print "--pcr_indel_model NONE"}') \
 -dt NONE
 
 ### QC ###
