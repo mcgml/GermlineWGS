723275de0b7b2a1412613cc174b057f0164a7ce3
mcgml <mcgml@cardiff.ac.uk>
2018-01-24 16:03:59 +0000

    added CNV calling

--- 1_GermlineWGS.sh
+++ 1_GermlineWGS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=999:00:00
 #PBS -l ncpus=12
+. /share/apps/root-distros/root_v5.34.36.Linux-slc6-x86_64-gcc4.4/bin/thisroot.sh
 set -euo pipefail
 PBS_O_WORKDIR=(`echo $PBS_O_WORKDIR | sed "s/^\/state\/partition1//" `)
 cd $PBS_O_WORKDIR
@@ -248,6 +249,41 @@ manta/runWorkflow.py \
 #soft link VCF
 ln -s manta/results/variants/diploidSV.vcf.gz "$seqId"_"$sampleId"_sv_filtered.vcf.gz
 ln -s manta/results/variants/diploidSV.vcf.gz.tbi "$seqId"_"$sampleId"_sv_filtered.vcf.gz.tbi
+
+#RD-CNV calling with CNVNator
+
+#extract reads
+/share/apps/CNVnator-distros/CNVnator_v0.3.3/src/cnvnator \
+-chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 \
+-root "$seqId"_"$sampleId".root \
+-tree "$seqId"_"$sampleId".bam \
+-unique
+
+#create histogram
+/share/apps/CNVnator-distros/CNVnator_v0.3.3/src/cnvnator \
+-chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 \
+-root "$seqId"_"$sampleId".root \
+-his 100 \
+-d /share/apps/CNVnator-distros/gatk_v0_hg38_split
+
+#stats
+/share/apps/CNVnator-distros/CNVnator_v0.3.3/src/cnvnator \
+-chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 \
+-root "$seqId"_"$sampleId".root \
+-stat 100
+
+#partition
+/share/apps/CNVnator-distros/CNVnator_v0.3.3/src/cnvnator \
+-chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 \
+-root "$seqId"_"$sampleId".root \
+-partition 100
+
+#calling
+echo -e "CNV_type\tcoordinates\tCNV_size\tnormalized_RD\te-val1\te-val2\te-val3\te-val4\tq0" > "$seqId"_"$sampleId"_cnv.txt
+/share/apps/CNVnator-distros/CNVnator_v0.3.3/src/cnvnator \
+-chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 \
+-root "$seqId"_"$sampleId".root \
+-call 100 >> "$seqId"_"$sampleId"_cnv.txt
 
 ### QC ###
 
